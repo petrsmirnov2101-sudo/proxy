23b04c46597755f0fd2d0efbc38a4d7eda5c935b
petrsmirnov2101-sudo <petr.smirnov2101@gmail.com>
2026-05-27 10:32:35 +0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,22 +1,18 @@
 export default async function handler(req, res) {
-  const targetUrl = req.query.url;
+  const { action, public_key, path } = req.query;
 
-  if (!targetUrl) {
-    return res.status(400).json({ error: 'Укажи ?url=ССЫЛКА' });
-  }
+  let url = 'https://cloud-api.yandex.net/v1/disk/public/resources';
+  if (action === 'download') url += '/download';
+
+  url += `?public_key=${encodeURIComponent(public_key)}`;
+  if (path) url += `&path=${encodeURIComponent(path)}`;
+  url += '&limit=100';
 
   try {
-    const response = await fetch(targetUrl, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0'
-      }
-    });
-
-    const contentType = response.headers.get('content-type');
-    const data = await response.arrayBuffer();
-
-    res.setHeader('Content-Type', contentType);
-    res.send(Buffer.from(data));
+    const response = await fetch(url);
+    const data = await response.json();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
